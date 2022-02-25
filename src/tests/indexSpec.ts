114b05bbb933106786fabb6ffb868e280fb7db17
newwebash <newtona2011@gmail.com>
2022-02-25 15:07:43 -0500

    Use async await in test and add failure status test

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -7,7 +7,17 @@ it('expect greeting to return "Hello Ashley!"', () => {
 });
 
 describe('GET /api', () => {
-    it('returns status 200', () => {
-        request(app).get('/api').expect(200);
+    it('returns status 200', async () => {
+        const res = await request(app).get('/api');
+
+        expect(res.statusCode).toEqual(200);
+    });
+
+    it('returns status 404', async () => {
+        const res = await request(app)
+            .post('/api')
+            .send({ exampeParam: 'a string' });
+
+        expect(res.statusCode).toEqual(404);
     });
 });
