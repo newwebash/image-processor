2787c54303c34ce5c9e0d94ca8ca8f86a4d697c6
newwebash <newtona2011@gmail.com>
2022-02-27 12:44:13 -0500

    Fix issue in test

--- src/tests/resizeImageSpec.ts
+++ src/tests/resizeImageSpec.ts
@@ -1,12 +1,10 @@
 import request from 'supertest';
 import app from '../app';
-const testImg = 'santamonica.jpg'
+const testImg = 'encenadaport.jpg'
 
 describe(`resizeImage GET /api/resizeImage`, () => {
-//describe(`resizeImage GET /api/resizeImage`, () => {
     it('returns an image when an image filepath is passed', async () => {
-        const res = await request(app).get(`/api/resizeImage?${testImg}`);
-        //console.log("🚀 ~ file: resizeImageSpec.ts ~ line 9 ~ it ~ res", res)
+        const res = await request(app).get(`/api/resizeImage?filename=${testImg}`);
 
         //expect(res.status).toEqual(200);
         expect(res.type).toEqual('image/jpeg')
@@ -14,7 +12,6 @@ describe(`resizeImage GET /api/resizeImage`, () => {
 
     it('returns an error when NO image filepath is passed', async () => {
         const res = await request(app).get(`/api/resizeImage`);
-        //console.log("🚀 ~ file: resizeImageSpec.ts ~ line 9 ~ it ~ res", res)
 
         expect(res.status).toEqual(400);
         //expect(res.type).toEqual('image/jpeg')
