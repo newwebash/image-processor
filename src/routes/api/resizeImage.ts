486f1988f4253ec4794f79d0085e2d1404b70bc5
newwebash <newtona2011@gmail.com>
2022-02-27 16:15:16 -0500

    Write failing test for size params and clean up code

--- src/routes/api/resizeImage.ts
+++ src/routes/api/resizeImage.ts
@@ -1,40 +1,34 @@
 import express from 'express';
 import sharp from 'sharp';
 const resizeImage = express.Router();
-const preProcessedPath = './application/pre_processed/'
-const processedPath = './application/processed/'
-//const img = 'fjord.jpg'
-//const resizedImg = `${processedPath}fjord-resized.jpg`
-
+const preProcessedPath = './application/pre_processed/';
+const processedPath = './application/processed/';
 
 resizeImage.get('/', (req, res) => {
     const resizeImage = async () => {
-      if (req.query.filename == null) {
-        res.status(400)
-        res.send("Error: no filename provided")
-        return
-      }
-      const img = req.query.filename as string
-      console.log("🚀 ~ file: resizeImage.ts ~ line 18 ~ resizeImage ~ typeof img", typeof img)
-      const resizedImg = img.replace('.', '-resized.')
-        try {
-          await sharp(`${preProcessedPath}${img}`)
-            .resize({
-              width: 200,
-              height: 200
-            })
-            .toFile(`${processedPath}${resizedImg}`)
-            res.sendFile(resizedImg , {root: processedPath})
-            console.log("done resizing");
-        } catch (error) {
-          console.log(`An error occurred during processing: ${error}`);
-          res.status(400)
-          res.send(`An error occurred during processing: ${error}`)
+        if (req.query.filename == null) {
+            res.status(400);
+            res.send('Error: no filename provided');
+            return;
         }
-      }
-      
-      resizeImage();
+        const img = req.query.filename as string;
+        const resizedImg = img.replace('.', '-resized.');
+        try {
+            await sharp(`${preProcessedPath}${img}`)
+                .resize({
+                    width: 200,
+                    height: 200,
+                })
+                .toFile(`${processedPath}${resizedImg}`);
+            res.sendFile(resizedImg, { root: processedPath });
+        } catch (error) {
+            console.log(`An error occurred during processing: ${error}`);
+            res.status(400);
+            res.send(`An error occurred during processing: ${error}`);
+        }
+    };
+
+    resizeImage();
 });
 
 export default resizeImage;
-
--- src/tests/resizeImageSpec.ts
+++ src/tests/resizeImageSpec.ts
@@ -1,20 +1,60 @@
 import request from 'supertest';
+import fsPromises from 'fs';
+import sharp from 'sharp';
 import app from '../app';
-const testImg = 'encenadaport.jpg'
+const testImg = 'encenadaport.jpg';
+const resizedTestImg = 'encenadaport-resized.jpg';
+const processedPath = './application/processed/';
+const testHeight = 400;
+const testWidth = 400;
 
 describe(`resizeImage GET /api/resizeImage`, () => {
-    it('returns an image when an image filepath is passed', async () => {
-        const res = await request(app).get(`/api/resizeImage?filename=${testImg}`);
-
-        //expect(res.status).toEqual(200);
-        expect(res.type).toEqual('image/jpeg')
+    beforeAll(() => {
+        const path = `${processedPath}${resizedTestImg}`;
+        if (fsPromises.existsSync(path)) {
+            try {
+                fsPromises.unlinkSync(path);
+            } catch (err) {
+                console.log(err);
+            }
+        }
     });
 
     it('returns an error when NO image filepath is passed', async () => {
         const res = await request(app).get(`/api/resizeImage`);
 
         expect(res.status).toEqual(400);
-        //expect(res.type).toEqual('image/jpeg')
+    });
+
+    it('returns an image when an image filepath is passed', async () => {
+        const res = await request(app).get(
+            `/api/resizeImage?filename=${testImg}`
+        );
+
+        expect(res.type).toEqual('image/jpeg');
+    });
+
+    fit('places the specified image in the processed folder with the specficied image size', async () => {
+        const res = await request(app).get(
+            `/api/resizeImage?filename=${testImg}`
+        );
+
+        expect(
+            fsPromises.existsSync(`${processedPath}${resizedTestImg}`)
+        ).toEqual(true);
+
+        const getImgSize = async () => {
+            try {
+                const metadata = await sharp(
+                    `${processedPath}${resizedTestImg}`
+                ).metadata();
+
+                expect(metadata.height).toEqual(testHeight);
+                expect(metadata.width).toEqual(testWidth);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getImgSize();
     });
 });
-
